7aa96258b7a64991c97956f090d86be81ade966b
oh80 <ogawa-hajime-vs@ynu.jp>
2022-12-16 22:34:43 +0900

    add summary table

--- 04_analyze/mcmc_sample_visualize/code/analyze.R
+++ 04_analyze/mcmc_sample_visualize/code/analyze.R
@@ -17,10 +17,18 @@ main <- function(){
     lay_title_U()%>% 
     basics$save_my_plot( var_name = "U_hist",
                          folder_name = "mcmc_sample_visualize")
-
   
+  J_summary <- J_result %>% 
+    extract_beta() %>% 
+    summaraize()
   
-  return(U_hist)
+  U_summary <- U_result %>% 
+    extract_beta() %>% 
+    summaraize()
+  
+  summary_table <- master_tables(J_summary,U_summary) %>% 
+    plot_summary_table() %>% 
+    save_table()
 }
 
 
@@ -28,8 +36,7 @@ lay_basic_J<- function(input){
   output <- input$b %>% 
     dplyr::as_tibble() %>% 
     ggplot2::ggplot(mapping = ggplot2::aes(x=value))+
-    ggplot2::xlim(-200,200)+
-    ggplot2::
+    ggplot2::xlim(-200,200)
   return(output)
 }
 
@@ -79,9 +86,50 @@ lay_title_U <- function(input){
 }
 
 
+extract_beta <- function(input){
+  output <- input$b %>% 
+    as.data.frame() %>% 
+    dplyr::as_tibble() %>% 
+    dplyr::rename("b"=".")
+
+  return(output)
+}
+
+summaraize <- function(input){
+  output <- input %>% 
+    dplyr::summarise(mean=mean(b),
+                     sd = sd(b),
+                     median=median(b),
+                     q_2.5=quantile(b,0.025),
+                     q_97.5=quantile(b,0.975))
+  return(output)
+}
+
+master_tables <- function(input_1,input_2){
+  output <- input_1 %>% 
+    dplyr::bind_rows(input_2) %>% 
+    dplyr::mutate("country"=c("JPN","USA")) %>% 
+    dplyr::select("country","mean","sd","q_2.5","q_97.5")
+  return(output)
+}
+
+
+plot_summary_table <- function(input){
+  output <- input %>% 
+    kableExtra::kbl(
+                    digits = 2,
+                    booktabs = TRUE,
+                    format = "latex") %>% 
+    kableExtra::kable_styling(font_size = 15)
+  return(output)
+}
+
+
+save_table <- function(input){
+  path <- "04_analyze/mcmc_sample_visualize/table/table.txt"
+  writeLines(input,
+             path)
+}
 box::use(`functions`/basics)
 main()
-
-
-  
 
